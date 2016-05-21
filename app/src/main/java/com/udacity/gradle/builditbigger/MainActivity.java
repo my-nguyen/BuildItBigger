a143fe46fe52eef27f2ca61873d03e8ddc26f849
iceguyvn <nguyen_my@yahoo.com>
2016-05-21 15:25:30 -0700

    Step 3: Create GCE Module

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,16 +1,25 @@
 package com.udacity.gradle.builditbigger;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
-import com.nguyen.JokeFactory;
+import com.example.my.myapplication.jokegcm.myApi.MyApi;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 import com.nguyen.activitylibrary.JokeActivity;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+    String mJoke = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +49,52 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Intent intent = JokeActivity.newIntent(this, JokeFactory.getJoke());
-        startActivity(intent);
+        new EndpointsAsyncTask().execute();
+        if (mJoke == null) {
+            Toast.makeText(this, "Jokes are not available at this moment. Please try again in a few seconds.", Toast.LENGTH_LONG);
+        } else {
+            Intent intent = JokeActivity.newIntent(this, mJoke);
+            startActivity(intent);
+        }
+    }
+
+    class EndpointsAsyncTask extends AsyncTask<Void, Void, String> {
+        private MyApi myApiService = null;
+
+        @Override
+        protected String doInBackground(Void... params) {
+            if(myApiService == null) {  // Only do this once
+                /*
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+                      // options for running against local devappserver
+                      // - 10.0.2.2 is localhost's IP address in Android emulator
+                      // - turn off compression when running against local devappserver
+                      .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                      // .setRootUrl("http://192.168.0.3:8080/_ah/api/")
+                      .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                          @Override
+                          public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                              abstractGoogleClientRequest.setDisableGZipContent(true);
+                          }
+                      });
+                      */
+                MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+                      .setRootUrl("https://udacity-4-1318.appspot.com/_ah/api/");
+                // end options for devappserver
+
+                myApiService = builder.build();
+            }
+
+            try {
+                return myApiService.sayHi("NGUYEN").execute().getData();
+            } catch (IOException e) {
+                return e.getMessage();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            mJoke = result;
+        }
     }
 }
--- jokejavalibrary/src/main/java/com/nguyen/JokeFactory.java
+++ jokejavalibrary/src/main/java/com/nguyen/JokeFactory.java
@@ -1,7 +1,25 @@
 package com.nguyen;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class JokeFactory {
+   static List<String> sJokes = new ArrayList<>();
+   static {
+      sJokes.add("How do I disable the autocorrect function on my wife?");
+      sJokes.add("Never get on one knee for a girl who won't get on two for you.");
+      sJokes.add("What's the difference between your wife and your job? After five years your job will still suck.");
+      sJokes.add("Any married man should forget his mistakes, there's no use in two people remembering the same thing.");
+      sJokes.add("It is much easier to apologize than to ask permission.");
+      sJokes.add("A good wife always forgives her husband when she's wrong.");
+      sJokes.add("What's the difference between a paycheck and a penis? You don't have to beg your wife to blow your paycheck.");
+      sJokes.add("I haven't spoken to my wife for 18 months- I don't like to interrupt her.");
+      sJokes.add("Childs experience: if a mother is laughing at the fathers jokes, it means they have guests.");
+      sJokes.add("Top 3 situations that require witnesses: 1) Crimes 2) Accidents 3) Marriages Need I say more?");
+   }
+
    public static String getJoke() {
-      return "How do I disable the autocorrect function on my wife?";
+      return sJokes.get(new Random().nextInt(sJokes.size()));
    }
 }
